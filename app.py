0cec7ccb334df1bb26362ac575804d27f370b9ab
Garrett Rodrigues <grod@grod-macbookpro.roam.corp.google.com>
2020-04-06 11:29:23 -0400

    personalize

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Sample code to test out google cloud's load balancing and CDN
 @my_flask_app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return "Hellooo World! from " + hostname
+    return "Hellooo World! from Garrett at " + hostname
 
 if __name__ == '__main__':
     my_flask_app.run("0.0.0.0", port=5000, debug=True)
